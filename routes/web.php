6ec2f552f3791e547b6962ed7bfc7d901b25c688
Facundo070 <FAKUDIAZ389@GMAIL.COM>
2025-11-25 18:43:25 -0300

    3.0

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/home', function () {
 
 Route::resource('usuarios', UserController::class)->middleware('auth');
 Route::get('/personas/{dni}', [App\Http\Controllers\PersonController::class, 'search']);
+
+// cambio de contraseña
+Route::middleware('auth')->group(function () {
+    Route::get('/perfil/cambiar-password', [App\Http\Controllers\ProfileController::class, 'editPassword'])->name('profile.password.edit');
+    Route::post('/perfil/cambiar-password', [App\Http\Controllers\ProfileController::class, 'updatePassword'])->name('profile.password.update');
+});
+
+
+// Route for daily resource PDF report
+Route::get('/reporte/recursos/pdf/dia', [PdfController::class, 'exportarRecursosPorDia'])->name('reporte.recursos.pdf.dia');
+
+// Route for resource PDF report by date range
+Route::get('/reporte/recursos/pdf/rango', [PdfController::class, 'exportarPorRangoDeFechas'])->name('reporte.pdf.rango');
+
+
